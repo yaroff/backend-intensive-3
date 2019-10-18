11bbacd7aac3f869dcb99b2e618708d82b3489df
Andrey Prisniak <aprisniak@lectrum.io>
2019-10-18 21:52:00 +0300

    feat(source): add midlewares

--- source/routers/users/index.js
+++ source/routers/users/index.js
@@ -3,10 +3,16 @@ import express from 'express';
 import { get, post } from './handlers';
 import { getByHash } from './hash';
 
+// Utils
+import { limiter, validator } from '../../utils';
+
+// Schemas
+import { createUser } from '../../schemas';
+
 export const router = express.Router();
 
-router.get('/', get);
-router.post('/', post);
+router.get('/', [ limiter(2, 1000 * 60) ], get);
+router.post('/', [ validator(createUser) ], post);
 
 router.get('/:userHash', getByHash);
 // router.put('/:userHash', updateByHash);
--- source/server.js
+++ source/server.js
@@ -1,10 +1,13 @@
 // Core
 import express from 'express';
+import bodyParser from 'body-parser';
 
 //Routers
 import * as routers from './routers';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: '10kb' }));
 
 app.use('/users', routers.users);
 
