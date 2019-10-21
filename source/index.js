7529cbb83d7c1cd011e65f0acb15aa4371dfac65
Andrey Prisniak <aprisniak@lectrum.io>
2019-10-21 18:28:19 +0300

    chore(source): update server.js file

--- source/index.js
+++ source/index.js
@@ -1,10 +1,13 @@
+// Core
+import debug from 'debug';
+
 // Instruments
 import { app } from './server';
 import { getPort } from './utils';
 
 const PORT = getPort();
+const dg = debug('server:main');
 
 app.listen(PORT, () => {
-    // eslint-disable-next-line
-    console.log(`Server API is up on port ${PORT}`);
+    dg(`Server API is up on port ${PORT}`);
 });
