70285577149ec10537cb7d7d4cb0e9339f0e9440
Andrey Prisniak <aprisniak@lectrum.io>
2019-10-28 18:42:52 +0200

    feat(source): add solution lesson-10-task-2

--- source/utils/index.js
+++ source/utils/index.js
@@ -2,3 +2,4 @@ export { getPort } from './env';
 export { limiter } from './limiter';
 export { validator } from './validator';
 export { authenticate } from './authenticate';
+export { logger } from './logger';
