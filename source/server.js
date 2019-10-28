566a40d798afce7d81d15867f6f8c98ad465d536
Andrey Prisniak <aprisniak@lectrum.io>
2019-10-28 19:10:11 +0200

    feat(source): add lesson-11-task-3

--- source/server.js
+++ source/server.js
@@ -2,6 +2,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// Instruments
+import { logger, errorLogger, NotFoundError } from './utils';
+
 //Routers
 import * as routers from './routers';
 
@@ -9,10 +12,44 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '10kb' }));
 
+// Logger
+if (process.env.NODE_ENV === 'development') {
+    app.use((req, res, next) => {
+        let body = null;
+
+        if (req.method !== 'GET') {
+            body = JSON.stringify(req.body, null, 2);
+        }
+
+        logger.debug(`${req.method} ${body ? `\n${body}` : ''}`);
+        next();
+    });
+}
+
 // Routers
 app.use('/auth', routers.auth);
 app.use('/users', routers.users);
 app.use('/classes', routers.classes);
 app.use('/lessons', routers.lessons);
+
+app.use('*', (req, res, next) => {
+    const error = new NotFoundError(
+        `Can not find right route for method ${req.method} and path ${req.originalUrl}`,
+    );
+    next(error);
+});
+
+if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-unused-vars
+    app.use((error, req, res, next) => {
+        const { name, message, statusCode } = error;
+        const errorMessage = `${name}: ${message}`;
+
+        errorLogger.error(errorMessage);
+
+        const status = statusCode ? statusCode : 500;
+        res.status(status).json({ message: message });
+    });
+}
 
 export { app };
