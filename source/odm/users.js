6b449eb8fc2308987d3d177197026468fbc6087e
Andrey Prisniak <aprisniak@lectrum.io>
2019-11-22 19:25:22 +0200

    feat(source): add users schema

--- source/odm/users.js
+++ source/odm/users.js
@@ -1,6 +1,75 @@
+// Core
 import mongoose from 'mongoose';
+import v4 from 'uuid/v4';
 
-const userSchema = new mongoose.Schema({ name: String });
-const users = mongoose.model('users', userSchema);
+const schema = new mongoose.Schema(
+    {
+        hash: {
+            type:     String,
+            required: true,
+            unique:   true,
+            default:  () => v4(),
+        },
+        name: {
+            first: {
+                type:     String,
+                required: true,
+            },
+            last: {
+                type:     String,
+                required: true,
+            },
+        },
+        emails: [
+            {
+                email: {
+                    type:     String,
+                    unique:   true,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        phones: [
+            {
+                phone: {
+                    type:     String,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        password: {
+            type:     String,
+            select:   false,
+            required: true,
+        },
+        sex: {
+            type:     String,
+            enum:     [ 'm', 'f' ],
+            required: true,
+        },
+        roles: [
+            {
+                type:    String,
+                default: 'newbie',
+                enum:    [ 'newbie', 'student', 'teacher' ],
+            },
+        ],
+        socials: {
+            facebook: String,
+            linkedin: String,
+            github:   String,
+            skype:    String,
+        },
+        notes:    String,
+        disabled: Boolean,
+    },
+    { timestamp: { createdAt: 'created', updatedAt: 'modified' } },
+);
+schema.index({ 'name.first': 1, 'name.last': 1 }, { name: 'flName' });
+schema.index({ notes: 'text' }, { name: 'notes' });
 
-export { users };
+export const users = mongoose.model('users', schema);
+
+users.createIndexes();
