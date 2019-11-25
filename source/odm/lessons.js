a54557ff901f9757908e0ffa6721da4e02df55c0
Andrey Prisniak <aprisniak@lectrum.io>
2019-11-25 17:25:55 +0200

    feat(source): add lesson-20-task-1 implementation

--- source/odm/lessons.js
+++ source/odm/lessons.js
@@ -2,53 +2,51 @@
 import mongoose from 'mongoose';
 import v4 from 'uuid/v4';
 
-const schema = new mongoose.Schema({
-    hash: {
-        type:     String,
-        required: true,
-        unique:   true,
-        default:  () => v4(),
-    },
-    title: {
-        type:     String,
-        required: true,
-    },
-    description: {
-        type:     String,
-        required: true,
-    },
-    order: {
-        type:     Number,
-        required: true,
-    },
-    availability: [
-        {
-            type: String,
-            enum: [ 'standard', 'select', 'premium' ],
+const schema = new mongoose.Schema(
+    {
+        hash: {
+            type:     String,
+            required: true,
+            unique:   true,
+            default:  () => v4(),
         },
-    ],
-    content: {
-        videos: [
+        title: {
+            type:     String,
+            required: true,
+        },
+        description: {
+            type:     String,
+            required: true,
+        },
+        order: {
+            type:     Number,
+            required: true,
+        },
+        availability: [
             {
-                title: String,
-                order: Number,
-                uri:   String,
+                type: String,
+                enum: [ 'standard', 'select', 'premium' ],
             },
         ],
-        keynotes: [
-            {
-                title: String,
-                order: Number,
-                uri:   String,
-            },
-        ],
+        content: {
+            videos: [
+                {
+                    title: String,
+                    order: Number,
+                    uri:   String,
+                },
+            ],
+            keynotes: [
+                {
+                    title: String,
+                    order: Number,
+                    uri:   String,
+                },
+            ],
+        },
     },
-    created: {
-        type:    Date,
-        default: () => new Date(),
-    },
-    modified: Date,
-});
+    { timestamp: { createdAt: 'created', updatedAt: 'modified' } },
+);
 
 schema.index({ order: 1 }, { name: 'order' });
 
--- source/odm/users.js
+++ source/odm/users.js
@@ -2,73 +2,71 @@
 import mongoose from 'mongoose';
 import v4 from 'uuid/v4';
 
-const schema = new mongoose.Schema({
-    hash: {
-        type:     String,
-        required: true,
-        unique:   true,
-        default:  () => v4(),
-    },
-    name: {
-        first: {
+const schema = new mongoose.Schema(
+    {
+        hash: {
             type:     String,
             required: true,
+            unique:   true,
+            default:  () => v4(),
         },
-        last: {
-            type:     String,
-            required: true,
-        },
-    },
-    emails: [
-        {
-            email: {
-                type:     String,
-                unique:   true,
-                required: true,
-            },
-            primary: Boolean,
-        },
-    ],
-    phones: [
-        {
-            phone: {
+        name: {
+            first: {
                 type:     String,
                 required: true,
             },
-            primary: Boolean,
+            last: {
+                type:     String,
+                required: true,
+            },
         },
-    ],
-    password: {
-        type:     String,
-        select:   false,
-        required: true,
-    },
-    sex: {
-        type:     String,
-        enum:     [ 'm', 'f' ],
-        required: true,
-    },
-    roles: [
-        {
-            type:    String,
-            default: 'newbie',
-            enum:    [ 'newbie', 'student', 'teacher' ],
+        emails: [
+            {
+                email: {
+                    type:     String,
+                    unique:   true,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        phones: [
+            {
+                phone: {
+                    type:     String,
+                    required: true,
+                },
+                primary: Boolean,
+            },
+        ],
+        password: {
+            type:     String,
+            select:   false,
+            required: true,
         },
-    ],
-    socials: {
-        facebook: String,
-        linkedin: String,
-        github:   String,
-        skype:    String,
+        sex: {
+            type:     String,
+            enum:     [ 'm', 'f' ],
+            required: true,
+        },
+        roles: [
+            {
+                type:    String,
+                default: 'newbie',
+                enum:    [ 'newbie', 'student', 'teacher' ],
+            },
+        ],
+        socials: {
+            facebook: String,
+            linkedin: String,
+            github:   String,
+            skype:    String,
+        },
+        notes:    String,
+        disabled: Boolean,
     },
-    notes:    String,
-    disabled: Boolean,
-    created:  {
-        type:    Date,
-        default: () => new Date(),
-    },
-    modified: Date,
-});
+    { timestamp: { createdAt: 'created', updatedAt: 'modified' } },
+);
 schema.index({ 'name.first': 1, 'name.last': 1 }, { name: 'flName' });
 schema.index({ notes: 'text' }, { name: 'notes' });
 
